22c375aeb9eac6d75e1db3b24afef35a65409830
Анастасия Щербинина <nastya_jane@mail.ru>
2011-05-06 22:48:50 +0400

    Update qemu stepper.

--- qemu/qemu-stepper.h
+++ qemu/qemu-stepper.h
@@ -6,7 +6,7 @@ void qemu_stepper_free(CPUState * env);
 int qemu_stepper_step(CPUState *env);
 int qemu_stepper_data_prepare(CPUState *env, unsigned long int code_len, unsigned long int stack_len);
 void qemu_stepper_data_set(CPUState *env, const unsigned char *ptr, unsigned long int size);
-void qemu_stepper_entry_set(CPUState *env, unsigned long int entry);
+void qemu_stepper_entry_set(CPUState *env, unsigned long int entry, unsigned long int stack_offset);
 unsigned long int qemu_stepper_eip(CPUState *env);
 int qemu_stepper_read(CPUState *env, char *buff, unsigned long int size);
 void qemu_stepper_print_debug(CPUState *env);
